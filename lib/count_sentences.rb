e5a7f64ce16d8641b5ebcee13e9ffb1af47f8401
Levy <kipkoechlevy096@gmail.com>
2023-02-10 17:11:37 +0300

    completed assignment

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,30 @@ require 'pry'
 class String
 
   def sentence?
-    
-  end
+      
+    self.end_with? "."
+     
+   end 
 
   def question?
-
+      
+    self.end_with? "?"
+     
+   
   end
 
   def exclamation?
-
+      
+    self.end_with? "!"
+     
   end
-
+   
   def count_sentences
-
+     
+    self.split(/[.!?]+\s/).count
+     
+     
   end
+  
 end
+ 
